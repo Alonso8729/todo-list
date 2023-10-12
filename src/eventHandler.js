d17e69ba25d2a2ab955d39d385bdf6ee14f077bf
Alonso8729 <alon8729@gmail.com>
2023-10-12 16:25:05 +0300

    Implemented modals handling

--- src/eventHandler.js
+++ src/eventHandler.js
@@ -8,14 +8,54 @@ const eventHandler = (() => {
 
         document.addEventListener(click, (event) => {
             const { target } = event;
+            const selectedLink = document.querySelector('.selected-link')
+            const modalTitle = document.querySelector('.modal-title');
+            const linkIndex = parseInt(target.getAttribute('data-link-index'), 10);
 
-            //MODAL for add-project
-            if (target.classList.contains('modal-optional-btn'))
-                dom.handleModal();
+            //Modal for adding project
+            if (target.classList.contains('add-project-icon')) {
+                dom.handleModal('show', 'Add Project', 'add');
+            }
+            //Modal for deleting and editing project
+            else if (target.classList.contains('project-btn')) {
+                projectIndex = parseInt(target.getAttribute('data-project-index'), 10);
+                //EDIT MODAL
+                if (target.classList.contains('fa-pen-to-square')) {
+                    dom.handleModal('show', 'Edit Task', 'edit', projectIndex);
+                }
+                //DELETE MODAL
+                else if (target.classList.contains('fa-trash-can')) {
+                    dom.handleModal('show', 'Delete Task', 'delete', projectIndex);
+                }
+            }
+            //ADDING TASK MODAL
+            if (target.classList.contains('add-task-icon')) {
+                projectIndex = parseInt(target.getAttribute('data-project-index'), 10);
+                taskIndex = parseInt(target.getAttribute('data-task-index'), 10);
+                dom.handleModal('show', 'Add Task', 'add', projectIndex, taskIndex);
+            }
+            //Modal for delete, edit, info task
+            if (target.classList.contains('task-icon')) {
+                projectIndex = parseInt(target.getAttribute('data-project-index'), 10);
+                taskIndex = parseInt(target.getAttribute('data-task-index'), 10);
+                //EDIT MODAL
+                if (target.classList.contains('fa-pen-to-square')) {
+                    dom.handleModal('show', 'Edit Task', 'edit', projectIndex, taskIndex);
+                }
+                else if (target.classList.contains('fa-trash-can')) {
+                    dom.handleModal('show', 'Delete Task', 'delete', projectIndex, taskIndex);
+
+                }
+                else if (target.classList.contains('fa-circle-info')) {
+                    dom.handleModal('show', 'Task Info', 'info', projectIndex, taskIndex);
+                }
+
+
+        }
 
         })
-    }
+}
 
-})();
+}) ();
 
 export default eventHandler;
