880c421e005a22ea824c1e6412f0596d8440bb41
Alonso8729 <alon8729@gmail.com>
2023-10-15 13:48:58 +0300

    Added getTask and clickListener

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import dom from './dom.js';
 import eventHandler from './eventHandler.js';
 
-//eventHandler.clickListener();
 dom.showProjects();
 dom.showMainTitle(0);
+dom.getTasks('all');
+
+eventHandler.clickListener();
